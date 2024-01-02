811b8d025eb2e4c6b5457b44392e9dce389f589e
Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-01-02 09:53:10 +0100

    Job02

---
+++ Jour01/Job02.py
@@ -0,0 +1,9 @@
+class Operation:
+    def __init__(self, nombre1=0, nombre2=0):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+ope = Operation(nombre1=12, nombre2 = 3)
+
+print("Le nombre est", ope.nombre1)
+print("Le nombre est", ope.nombre2)
