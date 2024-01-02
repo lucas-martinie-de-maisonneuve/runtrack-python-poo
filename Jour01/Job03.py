67c1d19d0bffbb739bd901eb5a35d26250d56eb5
Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-01-02 10:14:19 +0100

    Job03

---
+++ Jour01/Job03.py
@@ -0,0 +1,13 @@
+class Operation:
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+    def addition(self):
+        resultat = self.nombre1 + self.nombre2
+        return resultat
+
+ope = Operation(12, 3)
+print (f"Le résultat de l'addition est {ope.addition()}")
+
+ope.addition()
