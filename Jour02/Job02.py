ee410b1a8d936ab3731c7287bdd80562fa674f10
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-01-03 09:59:36 +0100

    Job02

---
+++ Jour02/Job02.py
@@ -0,0 +1,46 @@
+class Livre:
+    def __init__(self, titre, auteur, nombre_pages):
+        self.__titre = titre
+        self.__auteur = auteur
+        self.__nombre_pages = nombre_pages
+
+    def get_titre(self):
+        return self.__titre
+
+    def get_auteur(self):
+        return self.__auteur
+
+    def get_nombre_pages(self):
+        return self.__nombre_pages
+
+    def set_titre(self, new_titre):
+        self.__titre = new_titre
+
+    def set_auteur(self, new_auteur):
+        self.__auteur = new_auteur
+
+    def set_nombre_pages(self, new_nombre_pages):
+        if isinstance(new_nombre_pages, int) and new_nombre_pages > 0:
+            self.__nombre_pages = new_nombre_pages
+        else:
+            print("Erreur : Le nombre de pages doit être un entier positif.")
+
+livre = Livre(titre="Le python", auteur="Lucas", nombre_pages=100)
+
+print(f"""
+        Titre: {livre.get_titre()}
+        Auteur: {livre.get_auteur()}
+        Nombre de pages : {livre.get_nombre_pages()}
+""")
+
+livre.set_titre("Le python 2")
+livre.set_auteur("Martinie")
+
+livre.set_nombre_pages(-50)
+
+livre.set_nombre_pages(150)
+
+print(f"""
+        Nouveau Titre: {livre.get_titre()}
+        Nouvel Auteur: {livre.get_auteur()}
+        Nouveau nombre de pages : {livre.get_nombre_pages()}""")
